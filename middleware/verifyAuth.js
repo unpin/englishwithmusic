e31d81a8262afa9c9b060879c35919c57551c860
German <pinncho@gmail.com>
2021-04-17 15:03:06 +0600

    Send status code along with the Json response

--- middleware/verifyAuth.js
+++ middleware/verifyAuth.js
@@ -2,12 +2,16 @@ const jwt = require('jsonwebtoken')
 
 module.exports = (req, res, next) => {
     if (!req.headers.authorization) {
-        return res.status(401).send('Access denied. Please authenticate.')
+        return res
+            .status(401)
+            .send({ message: 'Access denied. Please authenticate.' })
     }
     const token = req.headers.authorization.replace('Bearer ', '')
     jwt.verify(token, process.env.JWT_SECRET, (error, decoded) => {
         if (error) {
-            return res.send('Access denied. Authentication Token is not valid.')
+            return res.status(401).send({
+                message: 'Access denied. Authentication Token is not valid',
+            })
         }
         req.user = decoded
         req.token = token
