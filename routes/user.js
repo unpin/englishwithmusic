565c72fb1791b50b662892a80512f69c7e9886e9
German <pinncho@gmail.com>
2021-04-23 22:27:07 +0600

    Add user route

---
+++ routes/user.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const router = express.Router()
+const bcrypt = require('bcrypt')
+const User = require('../models/User')
+
+router.post('/signup', async (req, res) => {
+    try {
+        const { name, email, password } = req.body
+        if (await User.findOne({ email })) {
+            return res.status(409).send({ message: 'Email already in use' })
+        }
+        const hashedPassword = await bcrypt.hash(password, 10)
+        const user = new User({ name, email, password: hashedPassword })
+        await user.save()
+        const publicProfile = user.getPublicProfile()
+        publicProfile.token = user.generateAuthToken()
+        res.status(201).json(publicProfile)
+    } catch (e) {
+        res.status(500).send({ message: 'Something went wrong.' })
+    }
+})
+
+router.post('/signin', async (req, res) => {
+    const { email, password } = req.body
+    const user = await User.findOne({ email })
+    if (user) {
+        bcrypt.compare(password, user.password).then((result) => {
+            if (result) {
+                const publicProfile = user.getPublicProfile()
+                publicProfile.token = user.generateAuthToken()
+                return res
+                    .header('Authorization', `Bearer ${publicProfile.token}`)
+                    .status(200)
+                    .send(publicProfile)
+            } else {
+                return res.status(401).send({ message: 'Incorrect password' })
+            }
+        })
+    } else {
+        res.status(401).send('User with this email has not been found')
+    }
+})
+
+module.exports = router
--- startup/routes.js
+++ startup/routes.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const cors = require('cors')
+const userRoute = require('../routes/user')
 
 module.exports = function (app) {
     app.use(cors())
     app.use(express.json())
     app.use(express.urlencoded({ extended: false }))
+    app.use('/api/users', userRoute)
 }
